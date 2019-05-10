358247661631b4becc1f293eb322eccfff5bfc04
Shendidy <sherif@shendidy.com>
2019-05-10 05:55:47 +0100

    pascal now returns pascal line 0

--- lib/pascal.rb
+++ lib/pascal.rb
@@ -1,5 +1,7 @@
 class Pascal
   def pascal(num)
-    [0]
+    return [0] unless num.is_a? Integer
+    return [0] unless num >= 0
+    return [1]
   end
 end
--- spec/pascal_spec.rb
+++ spec/pascal_spec.rb
@@ -2,8 +2,28 @@ require 'pascal'
 
 describe Pascal do
   context '1- pascal can know if a given parameter is not a valid' do
-    it 'number' do
-      expect(subject.pascal(-1)).to eq ([0])
+    it 'a- number' do
+      expect(subject.pascal(-1)).to eq([0])
+    end
+
+    it 'b- character' do
+      expect(subject.pascal("a")).to eq([0])
+    end
+  end
+
+  context '2- pascal can know if a given parameter is a valid' do
+    it 'a- number' do
+      expect(subject.pascal(0)).to eq([1])
+    end
+  end
+
+  context '3- pascal can return the correct pascal array for line' do
+    it '0' do
+      expect(subject.pascal(0)).to eq([1])
+    end
+
+    it '1' do
+      expect(subject.pascal(0)).to eq([1, 1])
     end
   end
 end
